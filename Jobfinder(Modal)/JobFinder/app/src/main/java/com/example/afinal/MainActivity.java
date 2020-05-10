3b6b98c3fec2dcca8095982279a4d5564d687824
AlirezaKondori <alirezako82@gmail.com>
2020-05-10 16:50:29 -0700

    compleyion %45

--- Jobfinder(Modal)/JobFinder/app/src/main/java/com/example/afinal/MainActivity.java
+++ Jobfinder(Modal)/JobFinder/app/src/main/java/com/example/afinal/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean emp;
+
+    Intent intent;
+
     private Button button;
 
     GlobalClass globalClass;
@@ -28,10 +34,23 @@ public class MainActivity extends AppCompatActivity {
 
         globalClass = (GlobalClass) getApplicationContext();
 
+        final SwitchCompat switchCompat = findViewById(R.id.switch1);
+
         nameInput = (EditText) findViewById(R.id.editText);
         passwordInput = (EditText) findViewById(R.id.editText2);
 
         button = (Button) findViewById(R.id.puss);
+        switchCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switchCompat.isChecked()) {
+                    emp = true;
+                }
+                else {
+                    emp = false;
+                }
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,8 +63,12 @@ public class MainActivity extends AppCompatActivity {
     private void openActivity() {
         globalClass.setName(nameInput.getText().toString());
         password = Integer.parseInt(passwordInput.getText().toString());
-
-        Intent intent = new Intent(this, Main2Activity.class);
+        if (emp) {
+            intent = new Intent(this, Main3Activity.class);
+        }
+        else {
+            intent = new Intent(this, Main2Activity.class);
+        }
         startActivity(intent);
     }
 }
