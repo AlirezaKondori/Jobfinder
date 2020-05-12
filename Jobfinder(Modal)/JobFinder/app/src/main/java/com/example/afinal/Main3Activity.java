ce6aaea44dd48d657d8c908f9b010eb8935de10f
AlirezaKondori <alirezako82@gmail.com>
2020-05-11 19:47:20 -0700

    Tablayout added

--- Jobfinder(Modal)/JobFinder/app/src/main/java/com/example/afinal/Main3Activity.java
+++ Jobfinder(Modal)/JobFinder/app/src/main/java/com/example/afinal/Main3Activity.java
@@ -1,8 +1,12 @@
 package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        TabItem tabHome = findViewById(R.id.tabHome);
+        TabItem tabJobPostings = findViewById(R.id.jobPostings);
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
